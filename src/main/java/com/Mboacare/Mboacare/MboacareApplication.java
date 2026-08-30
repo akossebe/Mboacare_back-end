3faf067fae5865d52041a8b7bded135f5f6ed005
weezy <kolomsoakossebe@gmail.com>
2026-08-30 15:41:47 +0200

    Initial commit - creation du projet Mboacare

---
+++ src/main/java/com/Mboacare/Mboacare/MboacareApplication.java
@@ -0,0 +1,13 @@
+package com.Mboacare.Mboacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MboacareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MboacareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mboacare/Mboacare/MboacareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mboacare.Mboacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MboacareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
